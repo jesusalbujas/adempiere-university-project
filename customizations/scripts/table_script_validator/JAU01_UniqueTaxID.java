3987f32c36d1e3af301f0630d20761f1f49942c4
jesusalbujas <jesusramirez35000@gmail.com>
2024-09-15 16:18:07 -0400

    Se agrega callout para validar Nro de Cédula único

---
+++ customizations/scripts/table_script_validator/JAU01_UniqueTaxID.java
@@ -0,0 +1,17 @@
+// groovy:JAU01_UniqueTaxID
+import org.compiere.model.PO;
+import org.compiere.model.Query;
+PO entity = A_PO;
+try {
+	String currentName = entity.get_ValueAsString("TaxID");
+    String tableName = entity.get_TableName();
+  if(currentName != null) {
+    int referenceId = new Query(A_Ctx, tableName, "UPPER(TRIM(TaxID)) = UPPER(TRIM(?)) AND " + tableName + "_ID <> ?", entity.get_TrxName()).setParameters(currentName, entity.get_ID()).setClient_ID().firstId();
+    if(referenceId > 0) {
+        return "@Error@ Número de Identificación ya existe";
+    }
+  }
+	result = "";
+} catch(Exception e) {
+	return "@Error@ " + e.getLocalizedMessage();
+}
