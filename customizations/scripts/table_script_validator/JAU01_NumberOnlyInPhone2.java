5529daa0c4f6cfd030e930caafdbcddcd8841531
jesusalbujas <jesusramirez35000@gmail.com>
2024-09-18 15:57:42 -0400

    fix: Se corrige validación para que solo donde sea proveedor no deje agregar texto en el campo `Phone`

--- customizations/scripts/table_script_validator/JAU01_NumberOnlyInPhone2.java
+++ customizations/scripts/table_script_validator/JAU01_NumberOnlyInPhone2.java
@@ -1,20 +1,28 @@
-// groovy: JAU01_NumberOnly
 import org.compiere.model.PO;
 PO entity = A_PO;
 
 try {
-    // Obtiene el valor actual del campo
-    String currentName = entity.get_ValueAsString("Phone2");
+    // Obtiene el valor actual del campo "isvendor"
+    boolean isVendor = entity.get_ValueAsBoolean("IsVendor");
 
-    // Verifica si el valor no es nulo
-    if (currentName != null) {
-        // Verifica si el valor contiene algo distinto a números
-        if (!currentName.matches('^[0-9]+$')) {
-            return "@Error@ Solo se permiten números.";
+    // Obtiene el valor actual del campo "isemployee"
+    boolean isEmployee = entity.get_ValueAsBoolean("IsEmployee");
+
+    // Solo realiza la validación si "IsVendor" es verdadero y "IsEmployee" es falso
+    if (isVendor && !isEmployee) {
+        // Obtiene el valor actual del campo "Phone"
+        String currentPhone = entity.get_ValueAsString("Phone2");
+
+        // Verifica si el valor no es null
+        if (currentPhone != null) {
+            // Verifica si el valor contiene letras
+            if (!currentPhone.matches('^[0-9]+$')) {
+                return "@Error@ Solo se permiten números en el campo teléfono.";
+            }
         }
     }
-    
-    // Si todo está bien, no retorna error
+
+    // Si todo está bien o las condiciones no se cumplend devolvera error
     result = "";
 
 } catch (Exception e) {
