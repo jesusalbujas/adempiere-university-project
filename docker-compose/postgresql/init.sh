1ef21bfdc59e7f7562a4791263c72584eeb28c91
jesusalbujas <jesusramirez35000@gmail.com>
2024-09-18 13:44:06 -0400

    Change init in database container

---
+++ docker-compose/postgresql/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+createdb -U adempiere keycloak
+pg_restore -U adempiere -d adempiere < /tmp/seed.backup -v
+pg_restore -U adempiere -d keycloak < /tmp/keycloak.backup -v
+
+
+AFTER_RUN_DIR="/tmp/after_run"
+if [ -d "$AFTER_RUN_DIR" ]; then
+    find "$AFTER_RUN_DIR" -maxdepth 1 -type f -name '*.sql' -print0 | while IFS= read -r -d '' file; do
+        echo "importing $file"
+        psql -U postgres < "$file"
+    done
+fi
