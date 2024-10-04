eac1267911a11ed44153f9abcf680896b3d7aeb8
jesusalbujas <jesusramirez35000@gmail.com>
2024-10-04 11:29:53 -0400

    update: Se corrige lógica para validar que un proveedor no exista en la misma empresa pero si en empresas diferentes

--- customizations/scripts/table_script_validator/groovy:JAU01_UniqueNameInVendorOnly.java
+++ customizations/scripts/table_script_validator/groovy:JAU01_UniqueNameInVendorOnly.java
@@ -1,32 +1,37 @@
-// groovy:JAU01_UniqueNameInVendorOnly
 import org.compiere.model.Query;
 import org.compiere.model.PO;
 PO entity = A_PO;
 
 try {
-    String currentName = entity.get_ValueAsString("Name");
+    String currentName = entity.get_ValueAsString("Name"); // Nombre del proveedor
+    String currentCompanyName = entity.get_ValueAsString("Name2"); // Nombre de la empresa
     String tableName = entity.get_TableName();
     
-    // Obtiene el valor actual del campo "isvendor"
+    // Obtiene el valor actual del campo "IsVendor"
     boolean isVendor = entity.get_ValueAsBoolean("IsVendor");
 
-    // Obtiene el valor actual del campo "isemployee"
+    // Obtiene el valor actual del campo "IsEmployee"
     boolean isEmployee = entity.get_ValueAsBoolean("IsEmployee");
 
     // Solo realiza la validación si "IsVendor" es verdadero y "IsEmployee" es falso
     if (isVendor && !isEmployee) {
-        // Obtiene el valor actual del campo
-        // Verifica si el valor no es null
-        if (currentName != null) {
-            // Verifica si el valor ya existe.
-            int referenceId = new Query(A_Ctx, tableName, "UPPER(TRIM(Name)) = UPPER(TRIM(?)) AND " + tableName + "_ID <> ?", entity.get_TrxName()).setParameters(currentName, entity.get_ID()).setClient_ID().firstId();
+        // Verifica si el nombre del proveedor y el nombre de la empresa no son null
+        if (currentName != null && currentCompanyName != null) {
+            // Verifica si el nombre del proveedor ya existe en la misma empresa
+            int referenceId = new Query(A_Ctx, tableName, 
+                "UPPER(TRIM(Name)) = UPPER(TRIM(?)) AND UPPER(TRIM(Name2)) = UPPER(TRIM(?)) AND " + tableName + "_ID <> ?", 
+                entity.get_TrxName())
+                .setParameters(currentName, currentCompanyName, entity.get_ID())
+                .setClient_ID()
+                .firstId();
+                
             if (referenceId > 0) {
-                return "@Error@ Nombre ya existe";
+                return "@Error@ Proveedor con este nombre ya existe en esta empresa";
             }
         }
     }
 
-    // Si todo está bien o las condiciones no se cumplen devolvera error
+    // Si todo está bien, no devuelve error
     result = "";
 
 } catch (Exception e) {
