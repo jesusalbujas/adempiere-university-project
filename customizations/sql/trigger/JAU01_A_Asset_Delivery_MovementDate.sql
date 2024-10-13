108d4a77c9da2825a73f3507a694686c8069ad01
jesusalbujas <jesusramirez35000@gmail.com>
2024-10-13 16:19:36 -0400

    Se agrega trigger para fecha de movimiento en histórico

---
+++ customizations/sql/trigger/JAU01_A_Asset_Delivery_MovementDate.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION update_asset_delivery_movementdate() 
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Actualizar el campo movementdate con el valor del campo created del nuevo registro
+    UPDATE a_asset_delivery
+    SET movementdate = NEW.created
+    WHERE a_asset_delivery_id = NEW.a_asset_delivery_id;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_update_asset_delivery_movementdate
+AFTER INSERT ON a_asset_delivery
+FOR EACH ROW
+EXECUTE FUNCTION update_asset_delivery_movementdate();
