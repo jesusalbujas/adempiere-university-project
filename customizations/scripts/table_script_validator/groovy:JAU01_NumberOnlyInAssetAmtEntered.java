1278fbf8d95d1c7b27132bad98358a61adc79df9
jesusalbujas <jesusramirez35000@gmail.com>
2024-10-12 20:05:22 -0400

    update: Se actualiza script para validar el campo `Cantidad` y `Valor`

--- customizations/scripts/table_script_validator/groovy:JAU01_NumberOnlyInAssetAmtEntered.java
+++ customizations/scripts/table_script_validator/groovy:JAU01_NumberOnlyInAssetAmtEntered.java
@@ -4,17 +4,29 @@ PO entity = A_PO;
 try {
     // Validar el campo "AssetAmtEntered" para asegurarse de que solo contenga números y puntos decimales
     String assetAmtEntered = entity.get_ValueAsString("AssetAmtEntered");
-
+    String aQty = entity.get_ValueAsString("A_QTY_Current")
     // Verifica si el valor de "AssetAmtEntered" no es null
     if (assetAmtEntered != null) {
         // Verifica si contiene solo números con un punto decimal opcional
         if (!assetAmtEntered.matches("^\\d+(\\.\\d+)?\$")) {
-            return "@Error@ El valor no puede contener caracteres especiales excepto puntos decimales, verifique e intente nuevamente.";
+            return "@Error@ El valor del activo  no puede contener caracteres especiales excepto puntos decimales, verifique e intente nuevamente.";
         }
 
         // Verifica que el valor no sea cero
         if (Double.parseDouble(assetAmtEntered) == 0) {
-            return "@Error@ El valor no puede ser cero, verifique los datos introducidos e intente nuevamente.";
+            return "@Error@ El valor del activo  no puede ser cero, verifique los datos introducidos e intente nuevamente.";
+        }
+    }
+
+    if (aQty != null) {
+        // Verifica si contiene solo números con un punto decimal opcional
+        if (!aQty.matches("^\\d+(\\.\\d+)?\$")) {
+            return "@Error@ La cantidad del activo no puede contener caracteres especiales excepto puntos decimales, verifique e intente nuevamente.";
+        }
+
+        // Verifica que el valor no sea cero
+        if (Double.parseDouble(aQty) == 0) {
+            return "@Error@ La cantidad del activo no puede ser cero, verifique los datos introducidos e intente nuevamente.";
         }
     }
 
