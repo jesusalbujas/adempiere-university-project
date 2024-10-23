d307b56c314cb82f1d1042e11bae72e851700ea6
jesusalbujas <jesusramirez35000@gmail.com>
2024-10-22 23:01:43 -0400

    .

--- customizations/sql/trigger/JAU01_Dar_De_Baja.sql
+++ customizations/sql/trigger/JAU01_Dar_De_Baja.sql
@@ -2,8 +2,6 @@ CREATE OR REPLACE FUNCTION update_asset_on_disposal()
 RETURNS TRIGGER AS
 $$
 BEGIN
-    -- Verificar si el campo 'docaction' es igual a "'C" (incluyendo la comilla simple literal)
-    IF NEW.docaction = '''C' THEN
         -- Actualizar los campos en la tabla a_asset
         UPDATE a_asset
         SET 
@@ -14,8 +12,6 @@ BEGIN
             A_Asset_Status = 'DI'
         WHERE 
             A_Asset_ID = NEW.A_Asset_ID; -- Suponiendo que A_Asset_ID sea la clave que relaciona ambas tablas
-    END IF;
-
     RETURN NEW;
 END;
 $$
