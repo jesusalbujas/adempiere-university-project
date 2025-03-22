367f2fc83307834efc8873071a0328aaec5ee4c7
jesusalbujas <jesusramirez35000@gmail.com>
2025-03-22 11:46:40 -0400

    Separate project in pst II and pst III

---
+++ PST-III/deployment/postgresql/after_run/001_tunning.sql
@@ -0,0 +1,26 @@
+ALTER SYSTEM SET
+ max_connections = '500';
+ALTER SYSTEM SET
+ shared_buffers = '1GB';
+ALTER SYSTEM SET
+ effective_cache_size = '3GB';
+ALTER SYSTEM SET
+ maintenance_work_mem = '256MB';
+ALTER SYSTEM SET
+ checkpoint_completion_target = '0.9';
+ALTER SYSTEM SET
+ wal_buffers = '16MB';
+ALTER SYSTEM SET
+ default_statistics_target = '100';
+ALTER SYSTEM SET
+ random_page_cost = '1.1';
+ALTER SYSTEM SET
+ effective_io_concurrency = '200';
+ALTER SYSTEM SET
+ work_mem = '524kB';
+ALTER SYSTEM SET
+ huge_pages = 'off';
+ALTER SYSTEM SET
+ min_wal_size = '1GB';
+ALTER SYSTEM SET
+ max_wal_size = '4GB';
---
+++ PST-III/deployment/postgresql/init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Creacion de la BD Keycloak
+createdb -U adempiere keycloak
+
+# Restaurar BD principal
+pg_restore -U adempiere -d adempiere < /tmp/seed.backup -v
+
+echo "BD Principal Restaurada"
+
+sleep 5
+# Restaurar BD de Keycloak
+pg_restore -U adempiere -d keycloak < /tmp/keycloak.backup -v
+echo "BD keycloak restaurada"
+
+sleep 5
+
+# createdb -U adempiere adempiere_test
+
+# pg_restore -U adempiere -d adempiere_test < /tmp/seed.backup -v
+
+# Aplicar tuneo a la bd
+AFTER_RUN_DIR="/tmp/after_run"
+if [ -d "$AFTER_RUN_DIR" ]; then
+    find "$AFTER_RUN_DIR" -maxdepth 1 -type f -name '*.sql' -print0 | while IFS= read -r -d '' file; do
+        echo "importing $file"
+        psql -U postgres < "$file"
+    done
+fi
